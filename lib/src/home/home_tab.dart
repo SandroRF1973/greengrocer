df75d5b1d1eb511ad7e64fc8050519cd37da2b81
Sandro Ferreira <sandrorf1973@gmail.com>
2023-11-14 10:44:08 -0300

    Aula - 21

--- lib/src/home/home_tab.dart
+++ lib/src/home/home_tab.dart
@@ -2,9 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 // ignore: library_prefixes
 import 'package:badges/badges.dart' as packageBadge;
+import 'package:greengrocer/src/home/components/category_tile.dart';
 
-class HomeTab extends StatelessWidget {
+// ignore: must_be_immutable
+class HomeTab extends StatefulWidget {
   const HomeTab({super.key});
+
+  @override
+  State<HomeTab> createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  List<String> categories = [
+    'Frutas',
+    'Grãos',
+    'Verduras',
+    'Temperos',
+    'Cereais',
+  ];
+
+  String selectedCategory = 'Frutas';
 
   @override
   Widget build(BuildContext context) {
@@ -88,11 +105,36 @@ class HomeTab extends StatelessWidget {
                 ),
               ),
             ),
-          )
+          ),
+
+          // Categorias
+          Container(
+            padding: const EdgeInsets.only(left: 25),
+            height: 40,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (_, index) {
+                return CategoryTile(
+                  onPressed: () {
+                    setState(() {
+                      selectedCategory = categories[index];
+                    });
+                  },
+                  category: categories[index],
+                  isSelected: categories[index] == selectedCategory,
+                );
+              },
+              separatorBuilder: (_, index) {
+                return const SizedBox(
+                  width: 10,
+                );
+              },
+              itemCount: categories.length,
+            ),
+          ),
         ],
       ),
-      // Campo de pesquisa
-      // Categorias
+
       // Grid
     );
   }
