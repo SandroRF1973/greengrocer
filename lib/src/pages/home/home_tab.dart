57c39b65f64e1e258834531091ab9f1c8c1c92e7
Sandro Ferreira <sandrorf1973@gmail.com>
2023-11-14 14:42:19 -0300

    Aula - 24

--- lib/src/pages/home/home_tab.dart
+++ lib/src/pages/home/home_tab.dart
@@ -4,6 +4,7 @@ import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:badges/badges.dart' as packageBadge;
 import 'package:greengrocer/src/config/app_data.dart' as app_data;
 import 'package:greengrocer/src/pages/home/components/category_tile.dart';
+import 'package:greengrocer/src/pages/home/components/item_tile.dart';
 
 // ignore: must_be_immutable
 class HomeTab extends StatefulWidget {
@@ -139,8 +140,8 @@ class _HomeTabState extends State<HomeTab> {
               ),
               itemCount: app_data.items.length,
               itemBuilder: (_, index) {
-                return Container(
-                  color: Colors.red,
+                return ItemTile(
+                  item: app_data.items[index],
                 );
               },
             ),
