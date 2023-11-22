0b582345f6628a879ef2c6c45a94bd2fc14b8c07
Sandro Ferreira <sandrorf1973@gmail.com>
2023-11-22 20:43:27 -0300

    Aula - 31

---
+++ lib/src/pages/cart/cart_tab.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class CartTab extends StatelessWidget {
+  const CartTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Carrinho'),
+      ),
+      body: Column(
+        children: [
+          const Expanded(
+            child: Placeholder(
+              color: Colors.red,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(30),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade300,
+                  blurRadius: 3,
+                  spreadRadius: 2,
+                ),
+              ],
+            ),
+            child: const Column(
+              children: [
+                Text(
+                  'Total geral',
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+                Text('50,00'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
