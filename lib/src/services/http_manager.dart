522670baa52b674ca3538f157335684f4979b3ca
Sandro Ferreira <sandrorf1973@gmail.com>
2023-12-27 08:37:49 -0300

    Aula - 80

---
+++ lib/src/services/http_manager.dart
@@ -0,0 +1,49 @@
+import 'package:dio/dio.dart';
+
+abstract class HttpMethods {
+  static String post = 'POST';
+  static String get = 'GET';
+  static String put = 'PUT';
+  static String patch = 'PATCH';
+  static String delete = 'DELETE';
+}
+
+class HttpManager {
+  Future<Map> restRequest({
+    required String url,
+    required String method,
+    Map? headers,
+    Map? body,
+  }) async {
+    // Headers da requisição
+    final defaultHeaders = headers?.cast<String, String>() ?? {}
+      ..addAll({
+        'content-type': 'application/json',
+        'accept': 'application/json',
+        'X-Parse-Application-Id': 'g1Oui3JqxnY4S1ykpQWHwEKGOe0dRYCPvPF4iykc',
+        'X-Parse-REST-API-Key': 'rFBKU8tk0m5ZlKES2CGieOaoYz6TgKxVMv8jRIsN',
+      });
+
+    Dio dio = Dio();
+
+    try {
+      Response response = await dio.request(
+        url,
+        options: Options(
+          headers: defaultHeaders,
+          method: method,
+        ),
+        data: body,
+      );
+
+      // Retorno do resultado do backend
+      return response.data;
+    } on DioException catch (error) {
+      // Retorno do erro do dio request
+      return error.response?.data ?? {};
+    } catch (error) {
+      // Retorno de map vazio para erro generalizado
+      return {};
+    }
+  }
+}
