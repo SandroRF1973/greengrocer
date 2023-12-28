e9fe9a15786ff2044033baa2fe52c1a2eb3e5caa
Sandro Ferreira <sandrorf1973@gmail.com>
2023-12-28 15:48:30 -0300

    Aula - 95

--- lib/src/pages/auth/view/sign_in_screen.dart
+++ lib/src/pages/auth/view/sign_in_screen.dart
@@ -2,6 +2,7 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:greengrocer/src/pages/auth/controller/auth_controller.dart';
+import 'package:greengrocer/src/pages/auth/view/components/forgot_password_dialog.dart';
 import 'package:greengrocer/src/pages/common_widgets/app_name_widget.dart';
 import 'package:greengrocer/src/pages/common_widgets/custom_text_field.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
@@ -131,7 +132,16 @@ class SignInScreen extends StatelessWidget {
                       Align(
                         alignment: Alignment.centerRight,
                         child: TextButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (_) {
+                                return ForgotPasswordDialog(
+                                  email: emailController.text,
+                                );
+                              },
+                            );
+                          },
                           child: Text(
                             'Esqueceu a senha',
                             style: TextStyle(color: CustomColors.customContrastColor),
