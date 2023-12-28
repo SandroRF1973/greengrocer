7a9e436c1a7dac57af8a7228ffb62e9a8dad7665
Sandro Ferreira <sandrorf1973@gmail.com>
2023-12-28 10:34:32 -0300

    Aula - 91

--- lib/src/pages/auth/view/sign_in_screen.dart
+++ lib/src/pages/auth/view/sign_in_screen.dart
@@ -6,6 +6,7 @@ import 'package:greengrocer/src/pages/common_widgets/app_name_widget.dart';
 import 'package:greengrocer/src/pages/common_widgets/custom_text_field.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:greengrocer/src/pages_routes/app_pages.dart';
+import 'package:greengrocer/src/services/validators.dart';
 
 class SignInScreen extends StatelessWidget {
   SignInScreen({super.key});
@@ -79,17 +80,7 @@ class SignInScreen extends StatelessWidget {
                         controller: emailController,
                         icon: Icons.email,
                         label: 'Email',
-                        validator: (email) {
-                          if (email == null || email.isEmpty) {
-                            return 'Digite seu email';
-                          }
-
-                          if (!email.isEmail) {
-                            return 'Digite um email válido';
-                          }
-
-                          return null;
-                        },
+                        validator: emailValidator,
                       ),
                       //Senha
                       CustomTextField(
@@ -97,17 +88,7 @@ class SignInScreen extends StatelessWidget {
                         icon: Icons.lock,
                         label: 'Senha',
                         isSecret: true,
-                        validator: (password) {
-                          if (password == null || password.isEmpty) {
-                            return 'Digite sua senha';
-                          }
-
-                          if (password.length < 7) {
-                            return 'Digite uma senha com pelo menos 7 caracteres';
-                          }
-
-                          return null;
-                        },
+                        validator: passwordValidator,
                       ),
                       SizedBox(
                         height: 50,
