ee61eaa2471927201282dcfe8b87cb1ad6365f58
Sandro Ferreira <sandrorf1973@gmail.com>
2023-12-29 09:12:38 -0300

    Aula - 98

--- lib/src/pages/home/view/home_tab.dart
+++ lib/src/pages/home/view/home_tab.dart
@@ -8,8 +8,8 @@ import 'package:badges/badges.dart' as packageBadge;
 import 'package:greengrocer/src/config/app_data.dart' as app_data;
 import 'package:greengrocer/src/pages/common_widgets/app_name_widget.dart';
 import 'package:greengrocer/src/pages/common_widgets/custom_shimmer.dart';
-import 'package:greengrocer/src/pages/home/components/category_tile.dart';
-import 'package:greengrocer/src/pages/home/components/item_tile.dart';
+import 'package:greengrocer/src/pages/home/view/components/category_tile.dart';
+import 'package:greengrocer/src/pages/home/view/components/item_tile.dart';
 import 'package:greengrocer/src/pages/home/controller/home_controller.dart';
 import 'package:greengrocer/src/services/utils_services.dart';
 
