77350f88fa2ac70b2ed511cbcd3f88c54b060030
Sandro Ferreira <sandrorf1973@gmail.com>
2023-12-30 10:53:43 -0300

    Aula - 103

--- lib/src/models/item_model.dart
+++ lib/src/models/item_model.dart
@@ -1,4 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:json_annotation/json_annotation.dart';
+
 part 'item_model.g.dart';
 
 @JsonSerializable()
@@ -24,4 +26,9 @@ class ItemModel {
   factory ItemModel.fromJson(Map<String, dynamic> json) => _$ItemModelFromJson(json);
 
   Map<String, dynamic> toJson() => _$ItemModelToJson(this);
+
+  @override
+  String toString() {
+    return 'ItemModel(id: $id, itemName: $itemName, imgUrl: $imgUrl, unit: $unit, price: $price, description: $description)';
+  }
 }
--- lib/src/pages/home/controller/home_controller.dart
+++ lib/src/pages/home/controller/home_controller.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:greengrocer/src/models/category_model.dart';
+import 'package:greengrocer/src/models/item_model.dart';
 import 'package:greengrocer/src/pages/home/repository/home_repository.dart';
 import 'package:greengrocer/src/pages/home/result/home_result.dart';
 import 'package:greengrocer/src/services/utils_services.dart';
@@ -28,6 +30,8 @@ class HomeController extends GetxController {
   void selectCategory(CategoryModel category) {
     currentCategory = category;
     update();
+
+    getAllProducts();
   }
 
   Future<void> getAllCategories() async {
@@ -52,5 +56,29 @@ class HomeController extends GetxController {
         );
       },
     );
+  }
+
+  Future<void> getAllProducts() async {
+    setLoading(true);
+
+    Map<String, dynamic> body = {
+      "page": 0,
+      "title": null,
+      "categoryId": "aGN3NQKlXp",
+      "itemsPerPage": 6,
+    };
+
+    HomeResult<ItemModel> result = await homeRepository.getAllProducts(body);
+
+    setLoading(false);
+
+    result.when(success: (data) {
+      debugPrint(data.toString());
+    }, error: (message) {
+      utilsServices.showToast(
+        message: message,
+        isError: true,
+      );
+    });
   }
 }
