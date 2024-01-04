b1b684e68ad75cc3e2facf23fde4bba3dc50d824
Sandro Ferreira <sandrorf1973@gmail.com>
2024-01-04 16:57:00 -0300

    Aula - 114

---
+++ lib/src/pages/cart/repository/cart_repository.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/constants/endpoints.dart';
+import 'package:greengrocer/src/services/http_manager.dart';
+
+class CartRepository {
+  final _httpManager = HttpManager();
+
+  Future getCartItems({
+    required String token,
+    required String userId,
+  }) async {
+    final result = await _httpManager.restRequest(
+      url: Endpoints.getCartItems,
+      method: HttpMethods.post,
+      headers: {
+        'X-Parse-Session-Token': token,
+      },
+      body: {
+        'user': userId,
+      },
+    );
+
+    if (result['result'] != null) {
+      debugPrint(result['result']);
+    } else {
+      debugPrint('Ocorreu um erro ao recuperar os itens do carrinho');
+    }
+  }
+}
