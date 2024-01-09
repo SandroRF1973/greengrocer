275576516b842d7023078019a10746dc2d797940
Sandro Ferreira <sandrorf1973@gmail.com>
2024-01-09 11:01:44 -0300

    Aula - 118

--- lib/src/pages/cart/controller/cart_controller.dart
+++ lib/src/pages/cart/controller/cart_controller.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:greengrocer/src/models/cart_item_model.dart';
+import 'package:greengrocer/src/models/item_model.dart';
 import 'package:greengrocer/src/pages/auth/controller/auth_controller.dart';
 import 'package:greengrocer/src/pages/cart/car_result/cart_result.dart';
 import 'package:greengrocer/src/pages/cart/repository/cart_repository.dart';
@@ -47,5 +48,38 @@ class CartController extends GetxController {
         );
       },
     );
+  }
+
+  int getItemIndex(ItemModel item) {
+    return cartItems.indexWhere((itemInList) => itemInList.id == item.id);
+  }
+
+  Future<void> addItemToCart({
+    required ItemModel item,
+    int quantity = 1,
+  }) async {
+    int itemIndex = getItemIndex(item);
+
+    if (itemIndex >= 0) {
+      // Já existe
+      cartItems[itemIndex].quantity += quantity;
+    } else {
+      cartRepository.addItemToCart(
+        userId: userId,
+        token: token,
+        productId: productId,
+        quantity: quantity,
+      );
+      // Não existe
+      cartItems.add(
+        CartItemModel(
+          id: '',
+          item: item,
+          quantity: quantity,
+        ),
+      );
+    }
+
+    update();
   }
 }
